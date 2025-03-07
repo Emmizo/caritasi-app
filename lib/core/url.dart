2aff8024865368a52ba49d077e9e9c16fea31bce
Emmizo <emmizokwizera@gmail.com>
2025-03-07 17:48:33 +0200

    code refactory

--- lib/core/url.dart
+++ lib/core/url.dart
@@ -1,4 +1,5 @@
 class Url {
   // static const urlData = "http://10.0.2.2:8000/api/v1";
-  static const urlData = "https://caritas.infinitexpedions.com/public/api/v1";
+  // static const urlData = "https://caritas.infinitexpedions.com/public/api/v1";
+  static const urlData = "http://localhost:8000/api/v1";
 }
